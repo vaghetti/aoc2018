daad850d79955554e369b863c65c879f5392f8fd
Douglas Vaghetti <douglasvaghetti@gmail.com>
2018-12-18 20:24:39 -0200

    Now with a mix project

---
+++ lib/aoc2018mix.ex
@@ -0,0 +1,18 @@
+defmodule Aoc2018mix do
+  @moduledoc """
+  Documentation for Aoc2018mix.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc2018mix.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aoc2018mix_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc2018mixTest do
+  use ExUnit.Case
+  doctest Aoc2018mix
+
+  test "greets the world" do
+    assert Aoc2018mix.hello() == :world
+  end
+end
